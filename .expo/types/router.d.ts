2cdc33e99fbfb75b4eb7bd90d7ad419930846d92
Georgi Markov <gogata1905@gmail.com>
2024-08-19 17:34:09 +0300

    add social component OAuth fro google auth

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -1,12 +1,12 @@
 /* eslint-disable */
-import * as Router from 'expo-router';
+import * as Router from "expo-router";
 
-export * from 'expo-router';
+export * from "expo-router";
 
-declare module 'expo-router' {
+declare module "expo-router" {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(auth)/welcome` | `/(root)` | `/(root)/(tabs)` | `/(root)/(tabs)/chat` | `/(root)/(tabs)/home` | `/(root)/(tabs)/profile` | `/(root)/(tabs)/rides` | `/(root)/chat` | `/(root)/home` | `/(root)/profile` | `/(root)/rides` | `/(tabs)` | `/(tabs)/chat` | `/(tabs)/home` | `/(tabs)/profile` | `/(tabs)/rides` | `/..\components\CustomButton` | `/..\components\InputField` | `/_sitemap` | `/chat` | `/home` | `/profile` | `/rides` | `/sign-in` | `/sign-up` | `/welcome`;
+      StaticRoutes: `/` | `/(auth)` | `/(auth)/sign-in` | `/(auth)/sign-up` | `/(auth)/welcome` | `/(root)` | `/(root)/(tabs)` | `/(root)/(tabs)/chat` | `/(root)/(tabs)/home` | `/(root)/(tabs)/profile` | `/(root)/(tabs)/rides` | `/(root)/chat` | `/(root)/home` | `/(root)/profile` | `/(root)/rides` | `/(tabs)` | `/(tabs)/chat` | `/(tabs)/home` | `/(tabs)/profile` | `/(tabs)/rides` | `/..\components\CustomButton` | `/..\components\InputField` | `/..\components\OAuth` | `/..\lib\auth` | `/_sitemap` | `/chat` | `/home` | `/profile` | `/rides` | `/sign-in` | `/sign-up` | `/welcome`;
       DynamicRoutes: never;
       DynamicRouteTemplate: never;
     }
