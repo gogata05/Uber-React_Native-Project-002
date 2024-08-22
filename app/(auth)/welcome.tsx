5d6dd81baca889fad53d678e054ce1feb93b9951
Georgi Markov <gogata1905@gmail.com>
2024-08-23 00:42:22 +0300

    edit welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,121 +1,43 @@
 import { router } from "expo-router";
 import { useRef, useState } from "react";
-import { Image, Text, TouchableOpacity, View, Button, StyleSheet } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 
-const onboardingData = [
-  {
-    id: 1,
-    title: "Welcome to the App",
-    description: "This is the first step in your journey.",
-    image: require("../assets/onboarding1.png")
-  },
-  {
-    id: 2,
-    title: "Learn New Skills",
-    description: "We help you develop new skills quickly and easily.",
-    image: require("../assets/onboarding2.png")
-  },
-  {
-    id: 3,
-    title: "Achieve Your Goals",
-    description: "Track your progress and achieve your goals with us.",
-    image: require("../assets/onboarding3.png")
-  }
-];
+import CustomButton from "@/components/CustomButton";
+import { onboarding } from "@/constants";
 
 const Home = () => {
   const swiperRef = useRef<Swiper>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
-  const isLastSlide = activeIndex === onboardingData.length - 1;
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
       <TouchableOpacity
         onPress={() => {
           router.replace("/(auth)/sign-up");
         }}
-        style={styles.skipButton}>
-        <Text style={styles.skipText}>Skip</Text>
+        className="w-full flex justify-end items-end p-5">
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
       </TouchableOpacity>
 
-      <Swiper ref={swiperRef} loop={false} dot={<View style={styles.dot} />} activeDot={<View style={styles.activeDot} />} onIndexChanged={index => setActiveIndex(index)}>
-        {onboardingData.map(item => (
-          <View key={item.id} style={styles.slide}>
-            <Image source={item.image} style={styles.image} resizeMode="contain" />
-            <View style={styles.titleContainer}>
-              <Text style={styles.title}>{item.title}</Text>
+      <Swiper ref={swiperRef} loop={false} dot={<View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />} activeDot={<View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />} onIndexChanged={index => setActiveIndex(index)}>
+        {onboarding.map(item => (
+          <View key={item.id} className="flex items-center justify-center p-5">
+            <Image source={item.image} className="w-full h-[300px]" resizeMode="contain" />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">{item.title}</Text>
             </View>
-            <Text style={styles.description}>{item.description}</Text>
+            <Text className="text-md font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">{item.description}</Text>
           </View>
         ))}
       </Swiper>
 
-      <Button title={isLastSlide ? "Get Started" : "Next"} onPress={() => (isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current?.scrollBy(1))} />
+      <CustomButton title={isLastSlide ? "Get Started" : "Next"} onPress={() => (isLastSlide ? router.replace("/(auth)/sign-up") : swiperRef.current?.scrollBy(1))} className="w-11/12 mt-10 mb-5" />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "space-between",
-    backgroundColor: "white"
-  },
-  skipButton: {
-    width: "100%",
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-    padding: 20
-  },
-  skipText: {
-    color: "black",
-    fontSize: 16,
-    fontWeight: "bold"
-  },
-  dot: {
-    width: 32,
-    height: 4,
-    marginHorizontal: 4,
-    backgroundColor: "#E2E8F0",
-    borderRadius: 2
-  },
-  activeDot: {
-    width: 32,
-    height: 4,
-    marginHorizontal: 4,
-    backgroundColor: "#0286FF",
-    borderRadius: 2
-  },
-  slide: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20
-  },
-  image: {
-    width: "100%",
-    height: 300
-  },
-  titleContainer: {
-    marginTop: 20,
-    flexDirection: "row",
-    justifyContent: "center"
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  description: {
-    fontSize: 16,
-    textAlign: "center",
-    color: "#858585",
-    marginTop: 10
-  }
-});
 
 export default Home;
