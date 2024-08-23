e5e3ea7012cae2ab9d6c0c36fc6bfd4b7f82a712
Georgi Markov <gogata1905@gmail.com>
2024-08-24 00:31:44 +0300

    rides updates

--- app/(root)/(tabs)/rides.tsx
+++ app/(root)/(tabs)/rides.tsx
@@ -13,33 +13,35 @@ const Rides = () => {
   const { data: recentRides, loading, error } = useFetch<Ride[]>(`/(api)/ride/${user?.id}`);
 
   return (
-    <FlatList
-      data={recentRides}
-      renderItem={({ item }) => <RideCard ride={item} />}
-      keyExtractor={(item, index) => index.toString()}
-      className="px-5"
-      keyboardShouldPersistTaps="handled"
-      contentContainerStyle={{
-        paddingBottom: 100
-      }}
-      ListEmptyComponent={() => (
-        <View className="flex flex-col items-center justify-center">
-          {!loading ? (
-            <>
-              <Image source={images.noResult} className="w-40 h-40" alt="No recent rides found" resizeMode="contain" />
-              <Text className="text-sm">No rides</Text>
-            </>
-          ) : (
-            <ActivityIndicator size="small" color="#000" />
-          )}
-        </View>
-      )}
-      ListHeaderComponent={
-        <>
-          <Text className="text-2xl font-JakartaBold my-5">All Rides</Text>
-        </>
-      }
-    />
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={recentRides}
+        renderItem={({ item }) => <RideCard ride={item} />}
+        keyExtractor={(item, index) => index.toString()}
+        className="px-5"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          paddingBottom: 100
+        }}
+        ListEmptyComponent={() => (
+          <View className="flex flex-col items-center justify-center">
+            {!loading ? (
+              <>
+                <Image source={images.noResult} className="w-40 h-40" alt="No recent rides found" resizeMode="contain" />
+                <Text className="text-sm">No recent rides found</Text>
+              </>
+            ) : (
+              <ActivityIndicator size="small" color="#000" />
+            )}
+          </View>
+        )}
+        ListHeaderComponent={
+          <>
+            <Text className="text-2xl font-JakartaBold my-5">All Rides</Text>
+          </>
+        }
+      />
+    </SafeAreaView>
   );
 };
 
